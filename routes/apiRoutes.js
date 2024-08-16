f6ad3ae46e98c5afd10e31ffd9349f614534209d
Betzaida Taylor <156623909+Betzaida96@users.noreply.github.com>
2024-08-16 00:11:04 -0600

    delete the notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -36,4 +36,28 @@ router.post('/notes', (req, res) => {
     });
 });
 
+//delete the notes
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    fs.readFile(path.join(__dirname, '../db/db.json'), 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed to read notes'});
+        }
+
+        let notes = JSON.parse(data);
+        notes = notes.filter( note => note.id !== noteId);
+
+        fs.writeFile(path.join(__dirname, '../db/db.json'), JSON.stringify(notes, null, 2), (err) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Failed to delete note'});
+            }
+
+            res.json({ message: 'Note deleted successfully'});
+        });
+    });
+});
+
 module.exports = router;
